784d3b76ded259cafc5af18e943fb7981c4837cc
Maga de magia <magali.learn@gmail.com>
2024-09-30 21:32:49 -0400

    Subir todas las tareas por el momento (Tareas 1 y 2)

---
+++ Tarea 2 - Java/MiInformacion.java
@@ -0,0 +1,15 @@
+public class MiInformacion {
+    public static void main(String[] args) {
+        String nombre = "Alba";
+        String apellido = "Martinez";
+        int edad = 20;
+        String paisResidencia = "Paraguay";
+        String ciudadResidencia = "San Lorenzo";
+        String pasatiempo = "bailar danza paraguaya";
+
+        // Impresiones de los datos
+        System.out.printf("Hola! Mi nombre es %s %s, tengo %d años\n",nombre,apellido,edad);
+        System.out.printf("Vivo en %s - %s\n",ciudadResidencia,paisResidencia);
+        System.out.printf("Mi pasatiempo favorito es %s\n", pasatiempo);
+    }
+}
